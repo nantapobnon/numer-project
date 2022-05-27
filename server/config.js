b715fbae3af5ad98bb40102a6d802ca78e6560e5
nantapobnon <74420029+nantapobnon@users.noreply.github.com>
2022-05-27 22:29:20 +0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,12 +1,5 @@
 const firebase=require('firebase');
 const firebaseConfig = {
-    apiKey: "AIzaSyCiSEdjzq_QecmnqL6GJZWJ0wRFp7KP0aw",
-    authDomain: "numer-baccc.firebaseapp.com",
-    projectId: "numer-baccc",
-    storageBucket: "numer-baccc.appspot.com",
-    messagingSenderId: "427079120842",
-    appId: "1:427079120842:web:03d93351ef697a29680133",
-    measurementId: "G-9NY78NKT2J"
   };
 firebase.initializeApp(firebaseConfig);
 const db=firebase.firestore();
